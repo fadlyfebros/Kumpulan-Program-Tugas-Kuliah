70913caca3e7f78a782e68e2f90ae00f8f78c2e5
fadly febro <110967878+fadlyfebros@users.noreply.github.com>
2023-11-09 23:10:19 +0700

    Add files via upload

---
+++ DataDiri_1402023024.java
@@ -0,0 +1,24 @@
+public class DataDiri_1402023024 {
+    public static void main(String[] args) {
+        String textName = "Halo, saya adalah mahasiswa Universitas Yarsi sebelumnya\n" +
+                "izin memperkenalkan diri terlebih dahulu melalui biodata dibawah ini";
+
+        String namaLengkap = "Nama Lengkap: " + args[0];
+        String alamat = "Alamat: " + args[1];
+        String prodi = "Prodi: " + args[2];
+        String angkatan = "Angkatan: " + args[3];
+        String tujuan = "Tujuan: " + args[4];
+        System.out.println("Data Diri: ");
+        System.out.println("Dly/1402023024");
+        System.out.println("---------------------------------------");
+        System.out.println(textName + "\n" + namaLengkap + "\n" + alamat + "\n" + prodi + "\n" + angkatan + "\n" + tujuan);
+		System.out.println("Salam kenal,");
+		System.out.println("******     **       **         **");
+		System.out.println("********   **        **       **");
+		System.out.println("********   **         **     **");
+		System.out.println("********   **            **");
+		System.out.println("********   **            **");
+		System.out.println("********   ********      **");
+		System.out.println("******     ********      **");
+    }
+}
